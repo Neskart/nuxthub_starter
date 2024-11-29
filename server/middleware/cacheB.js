bb43af184d254f35159e86329c44974801283676
Neskart <roell_florian@outlook.com>
2024-11-29 14:21:37 +0100

    Update cacheB.js

--- server/middleware/cacheB.js
+++ server/middleware/cacheB.js
@@ -1,6 +1,7 @@
 
 
 export default defineEventHandler((event) => {
+  /*
     // Set Cache-Control header for all responses
     setHeader(event, 'Cache-Control', 's-maxage=3600, stale-while-revalidate=600');
 
@@ -8,5 +9,15 @@ export default defineEventHandler((event) => {
     const path = event.node.req.url;
     if (path?.startsWith('/api')) {
         setHeader(event, 'Cache-Control', 's-maxage=1800, stale-while-revalidate=300'); // Example: API-specific cache
-    }
+    }*/
+  
+  const response = event.node.res;
+
+  // Set Cache-Control header
+  response.setHeader('Cache-Control', 'public, max-age=3600');
+
+  // Set Expires header
+  const futureDate = new Date();
+  futureDate.setHours(futureDate.getHours() + 1); // 1 hour in the future
+  response.setHeader('Expires', futureDate.toUTCString());
 });
